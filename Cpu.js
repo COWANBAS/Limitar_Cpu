2aa99d5a36b66ef4c4fc3caefa8abc94e2de5cab
Kauan Fonseca <131304556+COWANBAS@users.noreply.github.com>
2025-01-08 17:43:48 -0300

    Update Cpu.js

--- Cpu.js
+++ Cpu.js
@@ -1,26 +1,17 @@
 // ==UserScript==
 // @name            Folga na cpu
-// @description     Verifica se a guia do navegador está sendo usada, se não estiver limita o uso da cpu na guia
-// @version         2.0
+// @description     Verifica se a guia do navegador esta sendo usada, se nao estiver limita o uso da cpu na guia
+// @version         1.0
 // @author          Cowanbas
 // @match           *://*/*
 // @run-at          document-start
 // ==/UserScript==
 
 document.addEventListener("visibilitychange", function () {
-  if (document.visibilityState === "hidden") {
-    // Limitar o uso da CPU
-    if (window.requestIdleCallback) {
-      requestIdleCallback(function () {
-        // Reduzir uso da cpu quando a guia esta oculta
-      });
-    } else {
-      setTimeout(function () {
-        // Para navegadores que não suportam requestIdleCallback
-      }, 1000);
+    if (document.visibilityState == "hidden") {
+        document.documentElement.style.display = "none";
     }
-  }
-  else {
-
-  }
+    else {
+        document.documentElement.style.display = "block";
+    }
 });
