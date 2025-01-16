89987c3c120dde39a7bce36ca4d8b53dd24d5479
Kauan Fonseca <131304556+COWANBAS@users.noreply.github.com>
2025-01-16 12:26:23 -0300

    Add files via upload

---
+++ Cpu.2.0.js
@@ -0,0 +1,28 @@
+﻿// ==UserScript==
+// @name            Folga na cpu
+// @description     Verifica se a guia do navegador esta sendo usada, se nao estiver limita o uso da cpu na guia
+// @namespace       CowanCPU
+// @license         CowBas
+// @version         2.0
+// @author          Cowanbas
+// @match           *://*/*
+// @run-at          document-start
+// ==/UserScript==
+
+document.addEventListener("visibilitychange", function () {
+  if (document.visibilityState === "hidden") {
+    // Limitar o uso da CPU
+    if (window.requestIdleCallback) {
+      requestIdleCallback(function () {
+        // Reduzir uso da cpu quando a guia esta oculta
+      });
+    } else {
+      setTimeout(function () {
+        // Para navegadores que não suportam requestIdleCallback
+      }, 1000);
+    }
+  }
+  else {
+
+  }
+});
