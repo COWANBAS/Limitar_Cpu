cd46a360de7bbac87123679cd85428d3c81fcb31
Cowanbas <131304556+COWANBAS@users.noreply.github.com>
2026-01-22 10:21:21 -0300

    Update Cpu.2.0.js

--- Cpu.2.0.js
+++ Cpu.2.0.js
@@ -2,8 +2,8 @@
 // @name            Folga na cpu
 // @description     Verifica se a guia do navegador esta sendo usada, se nao estiver limita o uso da cpu na guia
 // @namespace       CowanCPU
-// @license         CowBas
-// @version         GPL-3.0
+// @license         GPL-3.0
+// @version         2.0
 // @author          Cowanbas
 // @match           *://*/*
 // @run-at          document-start
@@ -27,3 +27,4 @@ document.addEventListener("visibilitychange", function () {
   }
 
 });
+
